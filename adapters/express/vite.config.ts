737bf2f14033f6be70c6bacac23b89de14405ba0
Mees van Wel <mees.vanwel@hexa-it.nl>
2023-05-31 13:00:35 +0200

    fix: sitemap

--- adapters/express/vite.config.ts
+++ adapters/express/vite.config.ts
@@ -10,6 +10,13 @@ export default extendConfig(baseConfig, () => {
         input: ['src/entry.express.tsx', '@qwik-city-plan'],
       },
     },
-    plugins: [nodeServerAdapter({ name: 'express' })],
+    plugins: [nodeServerAdapter({ 
+      name: 'express',
+      ssg: {
+        include: ['/*'],
+        origin: 'https://www.hexa.center',
+        sitemapOutFile: 'sitemap.xml',
+      },
+    })],
   };
 });
